980e1bbd7bd0249cbf9e6bf3e989f993274f5d55
V <v.petrov@indev.expert>
2022-03-04 15:23:19 +0600

    Sync 0.0.1

--- expandable/src/main/java/kz/flyingv/expandablelinearlayout/ExpandableLinearLayout.kt
+++ expandable/src/main/java/kz/flyingv/expandablelinearlayout/ExpandableLinearLayout.kt
@@ -12,14 +12,11 @@ class ExpandableLinearLayout: LinearLayoutCompat {
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
         initExpandable(attrs)
     }
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
-        context,
-        attrs,
-        defStyleAttr
-    ){
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr){
         initExpandable(attrs)
     }
 
+    private var changesApplied = false
     private var expandedChildPosition = -1
 
     private fun initExpandable(attrs: AttributeSet?){
@@ -27,24 +24,56 @@ class ExpandableLinearLayout: LinearLayoutCompat {
         expandedChildPosition = attrs?.getAttributeIntValue("http://schemas.android.com/apk/res-auto", "expandedPosition", -1) ?: -1
     }
 
+
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
         super.onLayout(changed, l, t, r, b)
+        if(!changesApplied){
+            modifyChildren()
+            changesApplied = true
+        }
+    }
 
+    private fun modifyChildren(){
+        when(orientation){
+            HORIZONTAL -> {modifyChildrenHorizontal()}
+            VERTICAL -> {modifyChildrenVertical()}
+        }
+    }
+
+    private fun modifyChildrenHorizontal(){
+        Log.d("modifyChildren", "Parent width: $width")
+
+        var notExpandedChildrenWidth = 0
+        for(i in 0 until childCount){
+            if(i != expandedChildPosition){
+                Log.d("child", "$i child width: ${getChildAt(i).width}")
+                notExpandedChildrenWidth += getChildAt(i).width
+            }
+        }
+
+        Log.d("modifyChildren", "notExpandedChildrenWidth: $notExpandedChildrenWidth")
+        val params = getChildAt(expandedChildPosition).layoutParams
+        params.width = width - notExpandedChildrenWidth
+        getChildAt(expandedChildPosition).layoutParams = params
+    }
+
+    private fun modifyChildrenVertical(){
         Log.d("expandable child", "position: $expandedChildPosition")
-
         Log.d("onLayout", "Parent width: $width")
         Log.d("onLayout", "Parent height: $height")
 
-        val params = getChildAt(expandedChildPosition).layoutParams
-        params.width = 500
-        getChildAt(expandedChildPosition).layoutParams = params
+        var notExpandedChildrenHeight = 0
 
-        children.iterator().forEach { childView ->
-            Log.d("onLayout", "Child width: ${childView.width}")
+        for(i in 0 until childCount){
+            if(i != expandedChildPosition){
+                Log.d("child", "Parent width: $height")
+                notExpandedChildrenHeight += getChildAt(i).height
+            }
         }
 
-        Log.d("onLayout", "Children count: $childCount")
-
+        val params = getChildAt(expandedChildPosition).layoutParams
+        params.height = height - notExpandedChildrenHeight
+        getChildAt(expandedChildPosition).layoutParams = params
     }
 
 }
